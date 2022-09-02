842eb189804cca36c7ce7753cf17f7fb8325ef88
Marina Sereda <marina.gorb@gmail.com>
2022-09-02 23:30:46 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,27 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const itemClass = "task03__item";
+const imgClass = "task03__img";
+const galleryEl = document.querySelector(".gallery");
+
+function createGaleryMarkUp(images, itemClass, imgClass) {
+  return images.reduce((acc, img) => {
+    acc += `<li class ="${itemClass}"><img class = "${imgClass}" src="${img.url}" alt="${img.url}"></li>`;
+    return acc;
+  }, "");
+}
+
+galleryEl.insertAdjacentHTML("afterbegin", createGaleryMarkUp(images, itemClass, imgClass));
