75f415ca8d022693db1965132a21f7c78af109c9
Marina Sereda <marina.gorb@gmail.com>
2022-09-02 18:59:27 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsEl = document.querySelector("#ingredients");
+const className = "item";
+
+function createListItem(item, className) {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = item;
+  itemEl.classList.add(className);
+  return itemEl.outerHTML;
+}
+
+function getMarkUp(items, className) {
+  const markUp = items.reduce((acc, item) => {
+    acc += createListItem(item, className);
+    return acc;
+  }, "");
+  return markUp;
+}
+
+ingredientsEl.innerHTML = getMarkUp(ingredients, className);
