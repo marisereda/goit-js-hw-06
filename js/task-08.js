99df7f1a387eb6b4bb3eef49d57a4377def3df59
Marina Sereda <marina.gorb@gmail.com>
2022-09-03 16:51:11 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector(".login-form");
+const dataForm = {};
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  dataForm.email = email.value;
+  dataForm.password = password.value;
+  console.log(dataForm);
+  formEl.reset();
+}
