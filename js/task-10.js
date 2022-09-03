29fcccf82f72d9a8b19a49c32e42ebae94fe62fc
Marina Sereda <marina.gorb@gmail.com>
2022-09-03 22:21:20 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector("#controls input");
+const btnCreateEl = document.querySelector("[data-create]");
+const btnDestroyEl = document.querySelector("[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+btnCreateEl.addEventListener("click", createElement);
+btnDestroyEl.addEventListener("click", destroyElement);
+
+function createElement() {
+  destroyElement();
+  console.log(typeof inputEl.value);
+  let markUp = "";
+  let widthElement = 30;
+  let heightElement = 30;
+  for (let i = 0; i < Number(inputEl.value); i += 1) {
+    markUp += `<div style="width:${widthElement}px; height:${heightElement}px; margin-top: 10px; border: 1px solid grey; background-color:${getRandomHexColor()}"></div>`;
+    widthElement += 10;
+    heightElement += 10;
+  }
+
+  boxesEl.insertAdjacentHTML("afterbegin", markUp);
+}
+
+function destroyElement() {
+  boxesEl.innerHTML = "";
+}
