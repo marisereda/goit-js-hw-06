6c3fbe0c922c5674d8601d30b7311e6f41e3e51d
Marina Sereda <marina.gorb@gmail.com>
2022-09-03 18:07:25 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const textColorEl = document.querySelector(".color");
+const btnChangeClrEl = document.querySelector(".change-color");
+
+btnChangeClrEl.addEventListener("click", () => {
+  bodyEl.style.background = getRandomHexColor();
+  textColorEl.textContent = bodyEl.style.background;
+});
